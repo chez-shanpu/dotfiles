7e845eca4f2ad2cfaf54cacd859113274697fe9e
shanpu <cheztomo513@gmail.com>
2019-01-02 21:36:07 +0900

    update fish-config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,8 +1,14 @@
+# env vars
+set -x XDG_CONFIG_HOME $HOME/.config
+set -x XDG_CACHE_HOME $HOME/.cache
+
 set -x PATH $HOME/.anyenv/bin $PATH
 eval (anyenv init - | source)
 
+# pipenv
 set -x PIPENV_VENV_IN_PROJECT true
 
+# go
 set -x GOPATH ~/go
 set -x PATH $HOME/go/bin $PATH
 
@@ -11,15 +17,23 @@ alias ls 'ls -a --color=auto'
 alias .. 'cd ..'
 
 ## alias apt
-alias aptupd 'apt update'
-alias aptin 'apt install'
-alias aptsa 'apt search'
-alias aptls 'apt list'
+alias aud 'sudo apt update'
+alias ain 'sudo apt install'
+alias ase 'apt search'
+alias als 'apt list'
 
-## alias git+peco
-alias g 'cd (ghq root)/(ghq list | peco)'
+## alias nvim
+alias vi 'nvim'
+alias vim 'nvim'
 
-#peco
+
+#key-bindings
 function fish_user_key_bindings
+    #peco
     bind \cr peco_select_history
 end
+
+# decors/fish-ghq ctrl+gが何故か反応しない
+# set -U GHQ_SELECTOR peco
+# 代わり
+alias g 'ghq look (ghq list | peco)'
