4dd9bfaa795c5cbc64190190cdb96734f757245b
shanpu <cheztomo513@gmail.com>
2019-01-14 20:40:20 +0900

    Add xkeysnail config

---
+++ xkeysnail/config.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+import re
+from xkeysnail.transform import *
+
+
+define_multipurpose_modmap({
+    Key.CAPSLOCK: [Key.ESC, Key.LEFT_CTRL],
+    # SandS
+    Key.SPACE: [Key.SPACE, Key.LEFT_SHIFT],
+})
