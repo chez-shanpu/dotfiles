5bf07818cb0230573528fc1e2f4b937d34b32cff
chez-shanpu <cheztomo513@gmail.com>
2020-04-09 14:59:27 +0900

    Add provision.sh

---
+++ provision.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+DOTFILES_ROOT=$PWD
+
+ln -fs ${DOTFILES_ROOT}/.gitconfig ~
+ln -fs ${DOTFILES_ROOT}/.tmux.conf ~
+
+mkdir -p ~/.config/fish
+ln -fs ${DOTFILES_ROOT}/.config/fish/config.fish ~/.config/fish/
+ln -fs ${DOTFILES_ROOT}/.config/fish/fish_variables ~/.config/fish/
+ln -fs ${DOTFILES_ROOT}/.config/fish/fishfile ~/.config/fish/
+
+mkdir -p ~/.config/nvim
+ln -fs ${DOTFILES_ROOT}/.config/nvim/dein.toml ~/.config/nvim
+ln -fs ${DOTFILES_ROOT}/.config/nvim/dein_lazy.toml ~/.config/nvim
+ln -fs ${DOTFILES_ROOT}/.config/nvim/init.vim ~/.config/nvim
+
+mkdir -p ~/.config/powerline
+cp -r ${DOTFILES_ROOT}/.config/powerline ~/.config/powerline
